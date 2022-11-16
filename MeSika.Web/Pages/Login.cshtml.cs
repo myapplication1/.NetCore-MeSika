05e8c930dd2edb29dea72aad95e8fadc0fdad54a
gh.com Projects <amoahusa@gmail.com>
2022-11-16 06:40:31 -0500

    login

---
+++ MeSika.Web/Pages/Login.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MeSika.Web.Pages
+{
+    public class LoginModel : PageModel
+    {
+        private readonly ILogger<LoginModel> _logger;
+
+        public LoginModel(ILogger<LoginModel> logger)
+        {
+            _logger = logger;
+          
+        }
+    }
+        
+}
