f07e954c288d730b4419df1a4716986571aeccd1
koah <johndoe@example.comkamoah@icloud.com>
2022-11-17 09:25:35 -0500

    redirect if not loggefd in

--- MeSika.Web/Pages/Index.cshtml.cs
+++ MeSika.Web/Pages/Index.cshtml.cs
@@ -22,7 +22,7 @@ namespace MeSika.Web.Pages.Login
         public async Task<IActionResult> OnGet()
         {
             if (User.Identity.IsAuthenticated)
-                return Redirect("/Index");
+                return Redirect("/");
             else return Page();
 
         }
